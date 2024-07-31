d8f969e78425f21409c364d4ac0df24871db5fa5
Ted <65488381+tdwndr@users.noreply.github.com>
2024-07-31 22:09:15 +0100

    Tag config

--- tina/config.ts
+++ tina/config.ts
@@ -64,12 +64,12 @@ export default defineConfig({
             required: true,
             options: [
               {
-                value: '\u005B\u0027in brief\u0027\u005D',
-                label: '[in brief]',
+                value: "['in brief']",
+                label: 'in brief',
               },
               {
-                value: '\u005B\u0027in depth\u0027\u005D',
-                label: '[in depth]',
+                value: "['in depth']",
+                label: 'in depth',
               },
             ]
           },
